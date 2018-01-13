53685128b209bb95edbf72eb736e86e418a0d4bd
blechdom <blechdom@gmail.com>
2018-01-12 16:37:01 -0800

    added Group 1 and Group 2 functionality to DMX and working telnet/CasparCG demo

--- src/ControllersSetup.jsx
+++ src/ControllersSetup.jsx
@@ -1,120 +1,120 @@
-import React, {PropTypes} from 'react'
-import {Responsive, WidthProvider} from 'react-grid-layout';
-import lomap from 'lodash.map'
-import loresult from 'lodash.result'
-import lorange from 'lodash.range'
-import lorandom from 'lodash.random'
+import React from 'react';
 import 'isomorphic-fetch';
+import _ from 'lodash';
 import { Link } from 'react-router';
 import { Button, Glyphicon, Table, Panel } from 'react-bootstrap';
+import Toast from './Toast.jsx';
+import {Responsive, WidthProvider} from 'react-grid-layout';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-function generateLayout() {
-  return lomap( lorange(0, 25), function (item, i) {
-    var y = Math.ceil(Math.random() * 4) + 1;
-    return {
-      x: lorandom(0, 5) * 2 % 12,
-      y: Math.floor(i / 6) * y,
-      w: 2,
-      h: y,
-      i: i.toString(),
-      static: false,
-      sliderValue: 50
-    };
-  });
-}
-
 export default class ControllerSetup extends React.Component {
-
-  constructor(props, context){
-    super(props, context);
-	 this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
       currentBreakpoint: 'lg',
       compactType: 'vertical',
       mounted: false,
       layouts: {lg: this.props.initialLayout},
     };
- this.onNewLayout = this.onNewLayout.bind(this);  
 }
- handleSliderChange (event)  {
-
-   // this.setState({sliderValue: event.target.value});
-   // console.log(event.target.id + ': ' + this.state.i + ' ' + this.state.sliderValue);
-  }
-  componentDidMount() {
-    this.setState({
-      mounted: true
-    })
+componentDidMount() {
+    this.setState({mounted: true});
   }
 
   generateDOM() {
-
-    const styles = {
-      background: "#eee"
-    }
-
-return lomap(this.state.layouts.lg, (l, i) => {
+    return _.map(this.state.layouts.lg, function (l, i) {
       return (
-		  
-<div style={styles} key={i} className={l.static ? 'static': ''}>
-          {
-            l.static ? 
-              <span className="text" title="This item is static and can't be removed or resized">
-                static - {i}
-              </span> :
-              <span className="text">{i}</span> 
+        <div key={i} className={l.static ? 'static' : ''}>
+          {l.static ?
+            <span className="text" title="This item is static and cannot be removed or resized.">Static - {i}</span>
+            : <span className="text">{i}</span>
           }
-<div id="slidecontainer">
-                <input type="range" min="1" max="100" value={this.state.sliderValue} className="slider" id={i} ref={i} onChange={this.handleSliderChange}/>
-</div>
-        </div>
-      )
-    })
+        </div>);
+    });
   }
 
-  onBreakPointChange(breakpoint) {
+onBreakpointChange(breakpoint) {
+    console.log('onBreakpointChange breakpoint' + breakpoint);
     this.setState({
-      currentBreakPoint: breakpoint
-    })
+      currentBreakpoint: breakpoint,
+    });
   }
 
-  onLayoutChange(layout, layouts) {
-  console.log(layout, layouts);
-  }
+onCompactTypeChange() {
+    const {compactType: oldCompactType} = this.state;
+    const compactType = oldCompactType === 'horizontal' ? 'vertical' :
+                        oldCompactType === 'vertical' ? null : 'horizontal';
+    this.setState({compactType});
+ }
+
+ onLayoutChange(layout, layouts){
+ //   this.props.onLayoutChange(layout, layouts);
+//	this.onLayoutChange(layout,layouts); 
+console.log(layout, layouts);
+}
 
   onNewLayout() {
     this.setState({
-      layouts: {
-        lg: generateLayout()
-      }
-    })
+      layouts: {lg: generateLayout()}
+    });
   }
+render() {
 
-  render() {
-  return (
-      <div>
-        <div>Current Breakpoint: {this.state.currentBreakpoint} ({this.props.cols[this.state.currentBreakpoint]} columns)
+
+    return (
+        <div><div>
+	<div>
+          Current Breakpoint: {this.state.currentBreakpoint} ({this.props.cols[this.state.currentBreakpoint]} columns)
         </div>
-        <button onClick={this.onNewLayout}>Randomize Layout</button>
+        <div>Compaction type: {_.capitalize(this.state.compactType) || 'No Compaction'}</div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <button onClick={this.onCompactTypeChange}>Change Compaction Type</button>
         <ResponsiveReactGridLayout
           {...this.props}
           layouts={this.state.layouts}
           onBreakpointChange={this.onBreakpointChange}
           onLayoutChange={this.onLayoutChange}
- 	  measureBeforeMount={false}
-    	  useCSSTransforms={this.state.mounted}>
-          	{this.generateDOM()}
+	  measureBeforeMount={false}
+	  useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+         >
+          {this.generateDOM()}
         </ResponsiveReactGridLayout>
-      </div>
-    )
+	</div></div>
+
+    );
+
   }
+
+
 }
+
+ControllerSetup.propTypes = {
+//	onLayoutChange: React.PropTypes.func.isRequired,
+	params: React.PropTypes.object.isRequired,
+};
+
 ControllerSetup.defaultProps = {
     className: "layout",
-    isResizable: true,
     rowHeight: 30,
     onLayoutChange: function() {},
     cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs: 2},
     initialLayout: generateLayout()
-}; 
+};
+
+function generateLayout() {
+  return _.map(_.range(0, 25), function (item, i) {
+    var y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: _.random(0, 5) * 2 % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: Math.random() < 0.05
+    };
+  });
+}
+
